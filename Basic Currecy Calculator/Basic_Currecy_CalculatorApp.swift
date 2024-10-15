58c0f75703b8636e9400f1e63b9835370a167956
Abhishek Maurya <technicalyarana@gmail.com>
2024-10-15 20:28:51 +0530

    Initial Commit

---
+++ Basic Currecy Calculator/Basic_Currecy_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_Currecy_CalculatorApp.swift
+//  Basic Currecy Calculator
+//
+//  Created by Abhishek Maurya on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Basic_Currecy_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic Currecy CalculatorTests/Basic_Currecy_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_Currecy_CalculatorTests.swift
+//  Basic Currecy CalculatorTests
+//
+//  Created by Abhishek Maurya on 15/10/24.
+//
+
+import Testing
+@testable import Basic_Currecy_Calculator
+
+struct Basic_Currecy_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
